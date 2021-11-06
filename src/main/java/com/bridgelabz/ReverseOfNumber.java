e4a51e8df4f82ff102da2683d98f0b02654fb0d4
saranyellanki <saran.yellanki12@gmail.com>
2021-11-06 18:40:09 +0530

    Added:Palindrome Prime numbers

---
+++ src/main/java/com/bridgelabz/ReverseOfNumber.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+public class ReverseOfNumber {
+    /**
+     * Reverse of a number method reverse the given number
+     * @param number integer which should be reversed
+     * @return the integer reverse or updated value to the function
+     */
+    public static int reverseNumber(int number) {
+        int update = 0;
+        while (number > 0) {
+            int remainder = number % 10;
+            update = update * 10 + remainder;
+            number = number / 10;
+        }
+        return update;
+    }
+}
